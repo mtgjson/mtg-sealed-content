bc75f03067c83eeca997e7b6c19c992a0949a8c5
Sam Zimmerman <samzimmerman@me.com>
2023-09-14 22:01:44 -0700

    Move and rename script

---
+++ scripts/load_new_products.py
@@ -0,0 +1,59 @@
+import pathlib
+
+import ijson
+from tqdm import tqdm
+import yaml
+from pathlib import Path
+import os
+import os.path as path
+import time
+import requests
+
+
+def main():
+    url = "https://mtgjson.com/api/v5/AllPrintings.json"
+    r = requests.get(url, stream=True)
+
+    alt_codes = {"con_": "con"}
+    r_alt_codes = {"CON": "CON_"}
+
+    total = 0
+    complete = 0
+
+    codes = set()
+    all_sets = dict(ijson.kvitems(r.content, "data"))
+    for set_code, contents in tqdm(all_sets.items()):
+        output_file = (
+            Path("data/contents/")
+            .joinpath(r_alt_codes.get(set_code, set_code).upper())
+            .with_suffix(".yaml")
+        )
+        if output_file.is_file():
+            with open(output_file, "r") as f:
+                full = yaml.safe_load(f)
+            try:
+                empties = {k for k, v in full["products"].items() if not v}
+                products = {k: v for k, v in full["products"].items() if v}
+            except:
+                empties = set()
+                products = full
+        else:
+            empties = set()
+            products = {}
+        if "sealedProduct" not in contents:
+            continue
+        sealed_product = list(contents["sealedProduct"])
+        mtgjson_names = [p["name"] for p in sealed_product]
+        existing_names = [k for k in products.keys()]
+        total += len(mtgjson_names)
+        complete += len(existing_names)
+        for p in sealed_product:
+            if p["name"] not in products:
+                products[p["name"]] = []
+        if products:
+            with open(output_file, "w") as write:
+                yaml.dump({"code": set_code.lower(), "products": products}, write)
+
+
+if __name__ == "__main__":
+    main()
