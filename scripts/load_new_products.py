1dc8f907e64084509faf00b14dd8950745e363bc
Sam Zimmerman <samzimmerman@me.com>
2023-09-30 17:22:24 -0700

    Script tweaks

--- scripts/load_new_products.py
+++ scripts/load_new_products.py
@@ -13,7 +13,9 @@ import requests
 def get_cardKingdom():
     sealed_url = "https://api.cardkingdom.com/api/sealed_pricelist"
     r = requests.get(sealed_url)
-    return json.loads(r.content)
+    ck_data = json.loads(r.content)
+    return ck_data['meta']['base_url'], ck_data['data']
+
 
 def main():
     url = "https://mtgjson.com/api/v5/AllPrintings.json"
@@ -22,6 +24,7 @@ def main():
     alt_codes = {"con_": "con"}
     r_alt_codes = {"CON": "CON_"}
 
+    # Set up known product objects
     ck_no_url = dict()
     with open('data/ignore.yaml') as ignore_file:
         ignore = yaml.safe_load(ignore_file)
@@ -37,9 +40,9 @@ def main():
         ck_no_url.update({p['identifiers'].get('cardKingdomId', None):known_file for p in loaded_data['products'].values() if "cardKingdom" not in p.get('purchase_url', {})})
         ck_ids.update({p['identifiers'].get('tcgplayerProductId', None) for p in loaded_data['products'].values()})
     
-    ck_products = get_cardKingdom()
-    urlbase = ck_products['meta']['base_url']
-    for product in ck_products['data']:
+    # Load Card Kingdom products
+    urlbase, ck_products = get_cardKingdom()
+    for product in ck_products:
         if str(product['id']) in ck_ids or product['id'] in ck_ids:
             continue
         elif str(product['id']) in ck_no_url or product['id'] in ck_no_url:
@@ -55,10 +58,14 @@ def main():
                 yaml.dump(loaded_data, yfile)
         else:
             ck_review.update({product['name']: {"identifiers": {"cardKingdomId": str(product['id'])}}})
-        
+       
+    # Load TCGPlayer products [unknown]
+    
+    # Dump new products into the review section    
     with open('data/review.yaml', 'w') as yfile:
         yaml.dump({'cardKingdom': ck_review, 'tcgplayer': tg_review}, yfile)
     
+    # Add any new/modified products to the contents files
     for set_file in Path("data/products").glob("*.yaml"):
         with open(set_file, 'r') as yfile:
             load_data = yaml.safe_load(yfile)
@@ -75,50 +82,6 @@ def main():
             content_data['products'].pop(n)
         with open(Path("data/contents").joinpath(set_file.name), 'w') as yfile:
             yaml.dump(content_data, yfile)
-            
-def legacy():
-    codes = set()
-    all_sets = dict(ijson.kvitems(r.content, "data"))
-    for set_code, contents in all_sets.items():
-        output_file = (
-            Path("data/contents/")
-            .joinpath(r_alt_codes.get(set_code, set_code).upper())
-            .with_suffix(".yaml")
-        )
-        if output_file.is_file():
-            with open(output_file, "r") as f:
-                full = yaml.safe_load(f)
-            try:
-                empties = {k for k, v in full["products"].items() if not v}
-                products = {k: v for k, v in full["products"].items() if v}
-            except:
-                empties = set()
-                products = full
-        else:
-            empties = set()
-            products = {}
-        if "sealedProduct" not in contents:
-            continue
-        sealed_product = list(contents["sealedProduct"])
-        mtgjson_names = [p["name"] for p in sealed_product]
-        existing_names = [k for k in products.keys()]
-        total += len(mtgjson_names)
-        complete += len(existing_names)
-        for p in sealed_product:
-            if p["name"] not in products:
-                if p["name"] not in empties:
-                    with open("status.txt", 'a') as f:
-                        f.write(f"Added new product {set_code}/{p['name']}\n")
-                products[p["name"]] = []
-        '''
-        for n in existing_names:
-            if n not in mtgjson_names:
-                with open("status.txt", 'a') as f:
-                    f.write(f"Product {set_code}/{p['name']} no longer present\n")
-        '''
-        if products:
-            with open(output_file, "w") as write:
-                yaml.dump({"code": set_code.lower(), "products": products}, write)
 
 
 if __name__ == "__main__":
