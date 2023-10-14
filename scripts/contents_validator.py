bc8fce6b336ca1b5055eaa42b954ddebfd8b0fb8
Sam Zimmerman <samzimmerman@me.com>
2023-10-13 17:50:04 -0700

    Update a bunch of product types

--- scripts/contents_validator.py
+++ scripts/contents_validator.py
@@ -2,6 +2,24 @@ import yaml
 import product_classes as pc
 from pathlib import Path
 
+valid_categories = [
+    "BOOSTER_PACK", "BOOSTER_BOX", "BOOSTER_CASE", "DECK", "MULTI_DECK",
+    "DECK_BOX", "BOX_SET", "KIT", "BUNDLE", "BUNDLE_CASE", "DRAFT_SET",
+    "PRERELEASE_PACK", "PRERELEASE_CASE"
+]
+old_categories = [
+    "UNKNOWN", "CASE", "COMMANDER_DECK", "LAND_STATION", "TWO_PLAYER_STARTER_SET", "SUBSET"
+]
+valid_subtypes = [
+    "DEFAULT", "SET", "COLLECTOR", "JUMPSTART", "PROMOTIONAL", "THEME", "TOURNAMENT",
+    "WELCOME", "TOPPER", "PLANESWALKER", "CHALLENGE", "EVENT", "CHAMPIONSHIP",
+    "INTRO", "COMMANDER", "BRAWL", "ARCHENEMY", "PLANECHASE", "STARTER",
+    "TWO_PLAYER_STARTER", "DUEL", "CLASH", "BATTLE", "GAME_NIGHT", "FROM_THE_VAULT",
+    "SPELLBOOK", "SECRET_LAIR", "SECRET_LAIR_BUNDLE", "COMMANDER_COLLECTION",
+    "COLLECTORS_EDITION", "GUILD_KIT", "DECK_BUILDERS_TOOLKIT", "LAND_STATION",
+    "GIFT_BUNDLE", "FAT_PACK", "MINIMAL", "PREMIUM", "VIP", "DELUXE", "ADVANCED"
+]
+
 if __name__ == "__main__":
     contentFolder = Path("data/contents/")
     failed = False
@@ -32,9 +50,23 @@ if __name__ == "__main__":
             if "category" not in p.keys():
                 print(f"Product {name} in set {set_file.stem} missing category")
                 failed = True
+            elif p['category'] not in valid_categories:
+                if p['category'] in old_categories:
+                    #print(f"Product {name} uses an old category")
+                    pass
+                else:
+                    print(f"Product {name} has an invalid category")
+                    failed = True
             if "subtype" not in p.keys():
                 print(f"Product {name} in set {set_file.stem} missing subtype")
                 failed = True
+            elif p['subtype'] not in valid_subtypes:
+                if p['subtype'] == "UNKNOWN":
+                    #print(f"Product {name} missing a valid subtype")
+                    pass
+                else:
+                    print(f"Product {name} uses an invalid subtype")
+                    failed = True
     if failed:
         raise ImportError()
     print("All products validated")
