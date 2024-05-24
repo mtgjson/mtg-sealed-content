b0ddd83a81a9ff4e3d284c4aba79d18a6086f371
Sam Zimmerman <samzimmerman@me.com>
2024-05-23 18:49:16 -0700

    Updates May 23 (#183)
    * Some more error handling for failed product loading
    * So many products

--- scripts/load_new_products.py
+++ scripts/load_new_products.py
@@ -305,22 +305,29 @@ def load_cardtrader(secret):
 
         count = 0
         for blueprint in blueprints:
+            if not isinstance(blueprint, dict):
+                print(f"Product {blueprint} incorrectly formatted")
             if blueprint["game_id"] != 1:
                 continue
             if blueprint["category_id"] not in category_types:
                 continue
+            
+            if blueprint.get("version", ""):
+                product_name = blueprint["name"]+blueprint["version"]
+            else:
+                product_name = blueprint["name"]
 
-            if any(tag.lower() in blueprint["name"].lower() for tag in skip_tags):
+            if any(tag.lower() in product_name.lower() for tag in skip_tags):
                 continue
 
-            if "Secret Lair" in blueprint["name"]:
-                if any(tag.lower() in blueprint["name"].lower() for tag in sld_skip_tags):
+            if "Secret Lair" in product_name:
+                if any(tag.lower() in product_name.lower() for tag in sld_skip_tags):
                     continue
 
             count += 1
             sealed_data.extend([
                 {
-                    "name": blueprint["name"],
+                    "name": product_name,
                     "id": blueprint["id"],
                 }
             ])
@@ -485,13 +492,21 @@ def main(secret):
         except Exception as e:
             print(f"Could not load provider {key}")
             print(repr(e))
-            products = []
+            products = [{
+                "name": f"Could not load provider {key}",
+                "id": ""
+            }]
 
         for product in products:
             if str(product["id"]) in ids[key]:
                 continue
 
-            reviews[key][product["name"]] = {
+            prod_name = product["name"]
+            i = 0
+            while prod_name in reviews[key]:
+                i = i + 1
+                prod_name = product["name"] + str(i)
+            reviews[key][prod_name] = {
                 "identifiers": {provider["identifier"]: str(product["id"])},
                 "category": "UNKNOWN",
                 "subtype": "UNKNOWN"
