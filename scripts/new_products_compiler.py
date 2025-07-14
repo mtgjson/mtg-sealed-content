47b900c298cdb7e2d9ba7a4af22976de73fd3163
Sam Zimmerman <samzimmerman@me.com>
2025-07-13 21:07:06 -0700

    Update new_products_compiler.py

--- scripts/new_products_compiler.py
+++ scripts/new_products_compiler.py
@@ -2,6 +2,7 @@ import yaml
 import json
 from pathlib import Path
 
+date_required_subtypes = ["SECRET_LAIR", "SECRET_LAIR_BUNDLE"]
 
 def main(new_contents):
     products_new = {}
@@ -10,7 +11,7 @@ def main(new_contents):
             data = yaml.safe_load(f)
 
         for p_name, p_info in data["products"].items():
-            if (p_info["subtype"] in ["SECRET_LAIR", "SECRET_LAIR_BUNDLE"]) and "release_date" not in p_info:
+            if (p_info["subtype"] in date_required_subtypes) and "release_date" not in p_info:
                 with open("status.txt", 'a') as status_file:
                     status_file.write(f"Product {file.stem} - {p_name} missing required release date\n")
         products_new[data["code"]] = data["products"]
